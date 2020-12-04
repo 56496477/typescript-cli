6f68041f856e27fd1d51fda14c34df8faf0cfe6b
niufang <56496477@qq.com>
2020-12-04 16:02:00 +0800

    add..

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,16 +3,34 @@ module.exports = {
     extends: [
         'plugin:@typescript-eslint/recommended',
         'plugin:react/recommended',
-        'plugin:prettier/recommended'
+        'plugin:prettier/recommended',
+        'plugin:react-hooks/recommended'
     ],
     plugins: ['@typescript-eslint'],
+    env: {
+        browser: true,
+        es6: true,
+        amd: true,
+        browser: true
+    },
+    settings: {
+        react: {
+            version: 'detect' // React version. "detect" automatically picks the version you have installed.
+        }
+    },
     rules: {
         // 禁止使用 var
-        'no-var': "error",
+        'no-var': 'error',
         // 优先使用 interface 而不是 type
         '@typescript-eslint/consistent-type-definitions': [
-            "error",
-            "interface"
-        ]
+            'error',
+            'interface'
+        ],
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/camelcase': 'off',
+        'react/display-name': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        'react/prop-types': 'off',
+        'no-use-before-define': ['error', { variables: false }]
     }
-}
+};

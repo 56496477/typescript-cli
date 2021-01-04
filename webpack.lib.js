0679b6e46a3307d31060dd21d5485a508403e863
niufang <56496477@qq.com>
2021-01-04 10:40:48 +0800

    add..

--- webpack.lib.js
+++ webpack.lib.js
@@ -1,25 +1,39 @@
 const path = require('path');
 const CleanPlugin = require('clean-webpack-plugin');
+const UglifyJs = require('uglifyjs-webpack-plugin');
+const os = require('os');
+// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
     mode: process.env.NODE_ENV || 'development',
     entry: {
-        app: './src/app.jsx'
+        app: './src/view/index.tsx'
     },
     output: {
         path: path.resolve('./lib'),
         libraryTarget: 'umd',
-        library: 'TmsFormStage'
+        library: 'TmsLogin'
     },
     module: {
         rules: [
             {
-                test: /\.jsx?$/,
-                loader: 'babel!eslint',
+                test: /\.tsx?$/i,
+                use: [
+                    {
+                        loader: 'ts-loader'
+                    },
+                    process.env.node === 'production' && {
+                        loader: 'eslint',
+                        options: {
+                            fix: true
+                        }
+                    }
+                ].filter(Boolean),
                 include: [
                     path.resolve('./src'),
                     path.resolve('./node_modules/build-dev-server-client')
-                ]
+                ],
+                exclude: /node_modules/
             },
             {
                 test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
@@ -47,10 +61,6 @@ module.exports = {
                 loaders: ['style', 'css']
             },
             {
-                test: /\.scss$/,
-                loaders: ['style', 'css', 'sass']
-            },
-            {
                 test: /\.less/,
                 use: [
                     {
@@ -70,30 +80,56 @@ module.exports = {
             }
         ]
     },
+    devtool: 'cheap-module-source-map',
     resolve: {
-        extensions: ['.js', '.jsx', '.scss', '.css', 'less'],
+        extensions: ['.ts', '.tsx', '.js', '.css', 'less'],
         alias: {
             src: path.resolve('./src'),
+            model: path.resolve('./src/model'),
             view: path.resolve('./src/view'),
-            component: path.resolve('./src/view/component'),
-            model: path.resolve('./src/model')
+            component: path.resolve('./src/component'),
+            router: path.resolve('./src/router'),
+            assets: path.resolve('./public/assets'),
+            utils: path.resolve('./src/utils')
         }
+    },
+    optimization: {
+        minimizer: [
+            new UglifyJs({
+                parallel: os.cpus().length,
+                uglifyOptions: {
+                    compress: {
+                        drop_console: true,
+                        drop_debugger: true,
+                        pure_funcs: ['console.log'] // 移除console
+                    },
+                    output: {
+                        comments: false
+                    }
+                }
+            })
+        ]
     },
     resolveLoader: {
         moduleExtensions: ['-loader']
     },
     plugins: [
         new CleanPlugin([path.resolve('./lib')], { allowExternal: true })
+        // new BundleAnalyzerPlugin({
+        //     analyzerMode: 'static'
+        // })
     ],
     externals: {
-        react: 'react',
-        antd: 'antd',
-        moment: 'moment',
-        'react-redux': 'react-redux',
-        classnames: 'classnames',
-        redux: 'redux',
-        'react-router-dom': 'react-router-dom',
-        'redux-thunk': 'redux-thunk',
-        'react-popo': 'react-popo'
+        react: {
+            root: 'React',
+            commonjs2: 'react',
+            commonjs: 'react'
+        },
+        'react-dom': {
+            root: 'ReactDOM',
+            commonjs2: 'react-dom',
+            commonjs: 'react-dom'
+        },
+        antd: 'antd'
     }
 };
